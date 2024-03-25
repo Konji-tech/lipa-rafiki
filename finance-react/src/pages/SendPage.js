d1074fbaeaa34de65d56543b56582af95f79d2ee
Konji-tech <Konjiviola@gmail.com>
2024-03-25 03:58:58 +0300

    Added setAmount

--- finance-react/src/pages/SendPage.js
+++ finance-react/src/pages/SendPage.js
@@ -6,21 +6,22 @@ import { getContacts } from "../utils/cache";
 export default function SendPage() {
   const contacts = getContacts();
 
+  const [sendAmount, setAmount] = useState(0);
+
   // form state
 
   function handleSubmission(e) {
     // prevent page from reloading
     e.preventDefault();
+
+    setAmount(0);
   }
 
   return (
     <div className="flex justify-center items-center flex-col p-8 gap-8">
       <BalanceCard />
 
-      <form
-        onSubmit={handleSubmission}
-        class="w-full grid gap-4 p-4 bg-light-bg border-2 border-black rounded-xl"
-      >
+      <form onSubmit={handleSubmission} class="w-full grid gap-4 p-4 bg-light-bg border-2 border-black rounded-xl">
         <h1 class="text-xl font-bold font-mono uppercase">Send Money</h1>
 
         <div class="grid gap-2">
@@ -39,11 +40,8 @@ export default function SendPage() {
         </div>
 
         <div class="grid gap-2">
-          <label for="">Amount (KES)</label>
-          <input
-            type="number"
-            className="py-2 px-4 bg-white rounded-md border-2 border-black"
-          />
+          <label for="">Amount (KES){sendAmount}</label>
+          <input type="number" value={sendAmount} onChange={(e) => setAmount(e.target.value)} className="py-2 px-4 bg-white rounded-md border-2 border-black" />
         </div>
 
         <Button type="submit"> Initiate transactions </Button>
