8cf374dae2de6320f4c20e175f8103c68c4fd70d
TallestLegacy <tallestlegacy@gmail.com>
2024-05-20 20:24:56 +0300

    refactored exchange page

--- finance-react/src/pages/ExchangePage.js
+++ finance-react/src/pages/ExchangePage.js
@@ -6,35 +6,36 @@ import { useQuery } from "@tanstack/react-query";
 function ExchangePage() {
 	const exchangeQuery = useQuery({ queryKey: queryKeys.exchange });
 
-	// function to render the exchange rate table
-	const renderExchangeRateTable = () => {
-		const { rates } = exchangeQuery.data;
-
-		return (
-			<div className="px-4">
-				<div className="overflow-hidden rounded-xl border-2 border-black bg-primary">
-					<table className="w-full">
-						<thead className="border-b-2 border-black text-white">
-							<tr>
-								<th className="px-4 py-2 text-left">Currency</th>
-								<th className="px-4 py-2 text-left">Exchange Rate</th>
-							</tr>
-						</thead>
+	return (
+		<div className="px-8 py-4">
+			<div className="overflow-hidden rounded-xl border-2 border-black bg-primary">
+				<table className="w-full">
+					<thead className="border-b-2 border-black text-white">
+						<tr>
+							<th className="px-4 py-2 text-left">Currency</th>
+							<th className="px-4 py-2 text-left">Exchange Rate</th>
+						</tr>
+					</thead>
+					{exchangeQuery.data?.rates ? (
 						<tbody className="bg-light-bg">
-							{Object.entries(rates).map(([currency, rate]) => (
+							{Object.entries(exchangeQuery.data?.rates).map(([currency, rate]) => (
 								<tr key={currency}>
 									<td className="px-4 py-2">{currency}</td>
 									<td className="px-4 py-2">{rate}</td>
 								</tr>
 							))}
 						</tbody>
-					</table>
-				</div>
+					) : (
+						<tbody>
+							<tr>
+								<td colspan="2">Data not loaded</td>
+							</tr>
+						</tbody>
+					)}
+				</table>
 			</div>
-		);
-	};
-
-	return <div className="p-4">{renderExchangeRateTable()}</div>;
+		</div>
+	);
 }
 
 export default ExchangePage;
